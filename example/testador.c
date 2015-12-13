833866818d4db293aa892c8abae08faa95bd6418
Lorhan Sohaky <lorhansohaky@gmail.com>
2015-12-13 03:44:20 -0200

    Create testador.c

---
+++ example/testador.c
@@ -0,0 +1,15 @@
+#include<string.h>
+#include<stdbool.h>
+#include<stdio.h>
+
+int main(int argc, char *argv[] ) {
+	if(argc>1) {
+		if(strcmp(argv[1],"CASA")==0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	return 0;
+}
